70e4c9fdc155347db8b810e619f91de7208220b2
ranckprogram <1474574022@qq.com>
2022-01-26 18:25:40 +0800

    文档

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # echart-demo-getters
+
+拉取 https://www.makeapie.com/ 网站的爬虫，仅用于学习使用。
+
+
+代码里面的注释很清楚
+
+使用步骤
+
+```
+git clone git@github.com:ranckprogram/echart-demo-getters.git
+
+cd echart-demo-getters
+
+node index.js
+```
+
+然后等待2小时左右，有 18577 个资源  合计800M左右。可以修改代码中的**maxPage**减少下载量缩短下载时间
--- index.js
+++ index.js
@@ -1,102 +1,9 @@
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
+const { spawnSync } = require("child_process");
 
 const log = console.log;
-
-function saveAsJSON(filename, data, callback) {
-  fs.writeFile(
-    path.resolve(__dirname, "json", filename + ".json"),
-    data,
-    function (err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      log(filename, "下载成功！");
-      typeof callback === "function" && callback();
-    }
-  );
-}
-
-function parseDownloadTask(dir, filename, fn) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path.resolve(__dirname, dir, filename), function (err, data) {
-      // 读取文件失败/错误
-      if (err) {
-        throw err;
-      }
-      resolve(fn(data));
-    });
-  });
-}
-
-function downloader(url, filename, callback) {
-  const writeStream = fs.createWriteStream(
-    path.resolve(__dirname, "images", filename + ".png")
-  );
-  https.get(url, (res) => {
-    res.pipe(writeStream);
-    res.on("end", () => {
-      typeof callback === "function" && callback();
-    });
-  });
-}
-
-function createDownloader(dir, fileType) {
-  fileType = fileType ? `.${fileType}`: ""
-  return function downloader(url, filename, callback) {
-    const writeStream = fs.createWriteStream(
-      path.resolve(__dirname, dir, `${filename}${fileType}`)
-    );
-    https.get(url, (res) => {
-      res.pipe(writeStream);
-      res.on("end", () => {
-        typeof callback === "function" && callback();
-      });
- 
-    }).on('error', (e) => {
-      log("error");
-      log(url);
-      typeof callback === "function" && callback();    });;
-  };
-}
-
-function downloadMonitor(list, paramsFn, downloader, start = 0) {
-  let end = list.length;
-  if (start < end) {
-    if (!paramsFn(list[start])) {
-      return;
-    }
-    const { url, name } = paramsFn(list[start]);
-    downloader(url, name, () => {
-      log("下载完成", start);
-      downloadMonitor(list, paramsFn, downloader, start + 1);
-    });
-  } else {
-    log(start, end, "下载完成");
-  }
-}
-
-function downloadPage(pageSize, pageNum, maxPage) {
-  const url = `https://www.makeapie.com/chart/list?builtinTags%5B%5D=category-work&sortBy=rank&pageSize=${pageSize}&pageNumber=${pageNum}&author=all`;
-  https.get(url, (res) => {
-    var resultData = "";
-    res.on("data", (d) => {
-      process.stdout.write(d);
-      resultData += d;
-    });
-    res.on("end", () => {
-      const filename = "page" + pageNum;
-
-      saveAsJSON(filename, resultData, () => {
-        if (pageNum < maxPage) {
-          downloadPage(pageSize, pageNum + 1, maxPage);
-        }
-      });
-    });
-  });
-}
 
 function readdir(dir) {
   return new Promise((resolve, rejects) => {
@@ -109,70 +16,258 @@ function readdir(dir) {
   });
 }
 
+/**
+ * 解析目录读取json返回js对象
+ *
+ * @param {*} dir
+ * @param {*} filename
+ * @param {*} fn
+ * @return {*} Promise
+ */
+function parseDownloadTask(dir, filename, fn) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path.resolve(__dirname, dir, filename), function (err, data) {
+      if (err) {
+        throw err;
+      }
+      resolve(fn(data));
+    });
+  });
+}
+
+async function parseTaskByDir(dir, fn) {
+  const fileList = await readdir(dir);
+  const dataList = [];
+  for (const file of fileList) {
+    const data = await parseDownloadTask(dir, file, fn);
+    dataList.push(data);
+  }
+  return dataList;
+}
+
+async function exitsFolder(dir) {
+  const absPath = path.resolve(__dirname, dir);
+  try {
+    await fs.promises.stat(absPath);
+  } catch (e) {
+    await fs.promises.mkdir(absPath, { recursive: true });
+  }
+}
+
+/**
+ * 创建下载器。指定存放路径和文件类型
+ *
+ * @param {*} dir
+ * @param {*} fileType
+ * @return {*} downloader 下载器
+ *
+ * 下载器 downloader
+ * 下载出错时，会自动进入下一个
+ * TODO：考虑做个错误收集
+ *
+ * @param {*} url
+ * @param {*} filename
+ * @param {*} callback
+ */
+function createDownloader(dir, fileType) {
+  fileType = fileType ? `.${fileType}` : "";
+
+  return async function downloader(url, filename, callback) {
+    try {
+      await exitsFolder(dir);
+    } catch (e) {
+      throw Error(e.msg);
+    }
+    const writeStream = fs.createWriteStream(
+      path.resolve(__dirname, dir, `${filename}${fileType}`)
+    );
+    https
+      .get(url, (res) => {
+        res.pipe(writeStream);
+        res.on("end", () => {
+          typeof callback === "function" && callback();
+        });
+      })
+      .on("error", (e) => {
+        typeof callback === "function" && callback();
+      });
+  };
+}
+
+/**
+ * 控制异步顺序下载，一次下载一个，报错则下一个，直到下载完成，发出完成信号
+ *
+ * @param {*} list
+ * @param {*} paramsFn
+ * @param {*} downloader
+ * @param {number} [start=0]
+ * @return {*}
+ */
+function downloadMonitor(
+  list,
+  paramsFn,
+  downloader,
+  finishCallback = () => {},
+  start = 0
+) {
+  let end = list.length;
+  if (start < end) {
+    if (!paramsFn(list[start])) {
+      return;
+    }
+    const { url, name } = paramsFn(list[start]);
+    downloader(url, name, () => {
+      log(name, "下载完成", start);
+      downloadMonitor(list, paramsFn, downloader, finishCallback, start + 1);
+    });
+  } else {
+    log(start, end, "下载完成");
+    finishCallback();
+  }
+}
+
 function urlformatFilename(url, formart = "00000") {
   return url.replace(/\//g, formart);
 }
 
-async function main() {
-  const maxPage = 580;
-  const pageSize = 32;
-  let pageNum = 0;
+/**
+ *  根据分页获取每一页数据
+ *
+ */
+function getDateList(dir) {
+  return new Promise((resolve, reject) => {
+    const maxPage = 580;
+    const pageSize = 32;
+    let pageNum = 0;
 
-  // downloadPage(pageSize, pageNum, maxPage);
-
-  // const fileList = await readdir("json");
-  // const dataList = [];
-  // for (const file of fileList) {
-  //   const charts = await parseDownloadTask("json", file, function (data) {
-  //     return JSON.parse(data).data.charts;
-  //   });
-  //   dataList.push(...charts);
-  // }
-
-  // const dataDownloader = createDownloader("data", "json");
-  // const infoBaseUrl = "https://www.makeapie.com/chart/get"
-  // downloadMonitor(dataList, function (item) {
-  //   return {
-  //     url: `${infoBaseUrl}/${item.cid}`,
-  //     name: item.cid
-  //   }
-  // }, dataDownloader);
-
-  const dataJson = await readdir("data");
-  const dataDetailList = [];
-  for (const file of dataJson) {
-    const data = await parseDownloadTask("data", file, function (data) {
-      return JSON.parse(data).data;
+    const dataDownloader = createDownloader(dir, "json");
+    const dataList = Array.from({ length: maxPage }, function (_, index) {
+      return index + 1;
     });
-    if (data.externalScripts) {
-      dataDetailList.push(data);
-    }
-  }
+    downloadMonitor(
+      dataList,
+      function (item) {
+        pageNum = item;
+        const url = `https://www.makeapie.com/chart/list?builtinTags%5B%5D=category-work&sortBy=rank&pageSize=${pageSize}&pageNumber=${pageNum}&author=all`;
+        return {
+          url,
+          name: item,
+        };
+      },
+      dataDownloader,
+      function () {
+        resolve();
+      }
+    );
+  });
+}
 
-  let externalScriptList = dataDetailList
-    .map((item) => item.externalScripts.split(","))
-    .flat();
+function getAllThumbnail(dataList) {
+  return new Promise((resolve, reject) => {
+    const dataDownloader = createDownloader("images", "png");
+    downloadMonitor(
+      dataList,
+      function (item) {
+        return {
+          url: item.thumbnailURL,
+          name: item.cid,
+        };
+      },
+      dataDownloader,
+      function () {
+        resolve();
+      }
+    );
+  });
+}
 
-  const unrepeatedExternalScriptList = [...new Set(externalScriptList)]
-    .filter((item) => !item.includes("//"))
-    .filter((item) => item.includes(".js"));
+function getDataDetail(dataList) {
+  return new Promise((resolve, reject) => {
+    const infoBaseUrl = "https://www.makeapie.com/chart/get";
+    const dataDownloader = createDownloader("detail", "json");
 
-  // console.log(externalScriptList.length, unrepeatedExternalScriptList.length);
+    downloadMonitor(
+      dataList,
+      function (item) {
+        return {
+          url: `${infoBaseUrl}/${item.cid}`,
+          name: item.cid,
+        };
+      },
+      dataDownloader,
+      function () {
+        resolve();
+      }
+    );
+  });
+}
 
-  log(unrepeatedExternalScriptList.length);
-  const scriptDownloader = createDownloader("script", "");
-  const scriptBaseUrl = "https://gallerybox.makeapie.com";
-  downloadMonitor(
-    unrepeatedExternalScriptList,
-    function (item) {
-      console.log(`${scriptBaseUrl}${item}`, urlformatFilename(item));
-      return {
-        url: `${scriptBaseUrl}${item}`,
-        name: urlformatFilename(item),
-      };
-    },
-    scriptDownloader
-  );
+function getExternalScript(dataList) {
+  return new Promise((resolve, reject) => {
+    const scriptBaseUrl = "https://gallerybox.makeapie.com";
+    const dataDownloader = createDownloader("script", "");
+    downloadMonitor(
+      dataList,
+      function (item) {
+        return {
+          url: `${scriptBaseUrl}${item}`,
+          name: urlformatFilename(item),
+        };
+      },
+      dataDownloader,
+      function () {
+        resolve();
+      }
+    );
+  });
+}
+
+/**
+ * 1. 下载数据列表
+ * 2. 根据数据列表获取缩略图url下载缩略图
+ * 3. 根据数据列表获取id，拼接url下载详情
+ * 4. 根据详情解析额外脚本，下载非第三方脚本
+ */
+async function main() {
+  const dataListDir = "data";
+  const detailDir = "detail";
+
+  spawnSync("rm", ["-rf", "data", "detail", "images", "script"]);
+
+  getDateList(dataListDir)
+    .then(async () => {
+      const allPageDataList = (
+        await parseTaskByDir(dataListDir, function (data) {
+          return JSON.parse(data).data.charts;
+        })
+      ).flat();
+      return allPageDataList;
+    })
+    .then(async (allPageDataList) => {
+      await getAllThumbnail(allPageDataList);
+      return allPageDataList;
+    })
+    .then(async (allPageDataList) => {
+      await getDataDetail(allPageDataList);
+    })
+    .then(async () => {
+      const allDetailDataList = await parseTaskByDir(
+        detailDir,
+        function (data) {
+          return JSON.parse(data).data;
+        }
+      );
+
+      let externalScriptList = allDetailDataList
+        .map((item) => item.externalScripts.split(","))
+        .flat();
+
+      const unrepeatedExternalScriptList = [...new Set(externalScriptList)]
+        .filter((item) => !item.includes("//"))
+        .filter((item) => item.includes(".js"));
+
+      getExternalScript(unrepeatedExternalScriptList);
+    });
 }
 
 main();
